f13bec0ca1a3912a58712897a12026bad63fc028
Jaxon Penka <jaxonpenka127@gmail.com>
2026-07-28 21:01:41 -0400

    Fix redirects file by removing development paths

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' 
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react" 
+import { VitePWA } from "vite-plugin-pwa"
 
 export default defineConfig({
   plugins: [
@@ -8,12 +8,13 @@ export default defineConfig({
     VitePWA({
       registerType: "autoUpdate",
       injectRegister: "inline",
-      includeAssets: ['Images/**/*.{png,jpg,jpeg,svg,webp,gif,ico}'],
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
       devOptions: {
         enabled: true
       },
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,jpg,jpeg,svg,webp,gif}']
+        globPatterns: ["**/*.{js,css,html,ico,png,jpg,jpeg,svg,webp,gif,webmanifest}"],
+        maximumFileSizeToCacheInBytes: 3000000
       },
     })
   ],
